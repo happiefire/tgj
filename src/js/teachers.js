80f3f37940c89ca2438c50198e37d8984184d22a
Vincent Cheung <muse.vincent@gmail.com>
2014-07-25 15:30:12 +0800

    新增teachers.jade以及其样式，小幅修改gulpfile

---
+++ src/js/teachers.js
@@ -0,0 +1,28 @@
+(function(){
+  var app = angular.module('teachers', []);  
+  var data = [
+    {
+      groupName: '理科教研组',
+      members: [
+        {
+          uid: '392414124',
+          name: '孙佳骏',
+          classNum: 5,
+          subject: '数学'
+        },
+        {
+          uid:'392414642',
+          name: '张烁',
+          classNum: 3,
+          subject: '物理'
+        }
+      ]
+    }
+  ];
+
+  app.controller('TeachersController', function(){
+    this.groupIsOn = true;
+    this.groupsList = data;
+  });
+
+})();
